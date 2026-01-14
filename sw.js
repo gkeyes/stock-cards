7b0a8283b9b22ee56094290b74aa45fb71a96e52
DoraC <25716974+gkeyes@users.noreply.github.com>
2026-01-14 12:38:55 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Simple App Shell Service Worker */
-const CACHE_NAME = "stock-cards-20260114003meter";
+const CACHE_NAME = "stock-cards-20260114004meter";
 const ASSETS = [
   "./",
   "./index.html",
